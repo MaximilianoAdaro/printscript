6b3d90ac8ce4d72aaaaf4471da4cd7090f653002
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-03-18 01:05:47 -0300

    add dependencies

---
+++ interpreter/src/main/java/Dummy.java
@@ -0,0 +1,7 @@
+public class Dummy {
+
+    Dummy3 dummy3 = new Dummy3();
+
+    Dummy2 dummy2 = new Dummy2();
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        Dummy dummy = new Dummy();
+        Dummy2 dummy2 = new Dummy2();
+        Dummy3 dummy3 = new Dummy3();
+
     }
 }
