08060e0298f06260cacf6a1abf1a1bdc33f53044
Joaquin Iannuzzi <joaco.iannuzzi@gmail.com>
2021-03-30 12:47:24 -0300

    trying formatter

--- lexer/src/main/java/lexer/state/impls/SymbolState.java
+++ lexer/src/main/java/lexer/state/impls/SymbolState.java
@@ -22,12 +22,10 @@ public class SymbolState extends AbstractLexerState {
   @Override
   public LexerState nextValue(char c) {
     if (isNewline(c)) return new EmptyState(lexerContext.changeLine());
-
     if (isWhitespace(c)) return new EmptyState(lexerContext.reset());
     if (isNumber(c)) return new NumberState(lexerContext.reset(c));
     if (isAnySymbol(c)) return new SymbolState(lexerContext.reset(c));
     if (isStringSymbol(c)) return new StringState(lexerContext.reset(), c);
-
     if (isLetter(c)) return new TextState(lexerContext.reset(c));
 
     throw new IllegalStateException("Unexpected value: " + c);
