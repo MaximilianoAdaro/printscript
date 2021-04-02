5989314e92d031819c232dd28c98dbba5e5f9b9b
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-04-02 01:04:14 -0300

    add maps as variables and assignations

--- interpreter/src/main/java/interpreter/visitor/InterpretationVisitor.java
+++ interpreter/src/main/java/interpreter/visitor/InterpretationVisitor.java
@@ -2,27 +2,16 @@ package interpreter.visitor;
 
 import java.util.HashMap;
 import java.util.Map;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.val;
 import parser.node.impl.AssignationNode;
 import parser.node.impl.DeclarationNode;
 import parser.node.impl.IdentifierNode;
-import parser.node.impl.literalNodes.NumberLiteralValue;
-import parser.node.impl.literalNodes.StringLiteralValue;
 import parser.node.impl.literalNodes.TypeValue;
 import parser.node.interfaces.LiteralValue;
 
 public class InterpretationVisitor extends AbstractNodeVisitor {
 
-  @Data
-  @AllArgsConstructor
-  class Variable {
-    private TypeValue typeValue;
-    private LiteralValue literalValue;
-  }
-
-  private static Map<String, Variable> variables = new HashMap<>();
+  private static Map<String, TypeValue> variables = new HashMap<>();
+  private static Map<String, LiteralValue> assignations = new HashMap<>();
 
   @Override
   public void visit(IdentifierNode identifierNode) {
@@ -34,12 +23,7 @@ public class InterpretationVisitor extends AbstractNodeVisitor {
     final var variableName = declarationNode.getIdentifierNode().getValue();
     if (variables.containsKey(variableName)) throw new RuntimeException();
     final var variableType = declarationNode.getTypeValue();
-    val literalValue =
-        switch (variableType) {
-          case NUMBER -> new NumberLiteralValue(0);
-          case STRING -> new StringLiteralValue("");
-        };
-    variables.put(variableName, new Variable(variableType, literalValue));
+    variables.put(variableName, variableType);
   }
 
   @Override
