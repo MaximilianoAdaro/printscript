382e751f3a22ae0779a62937293f30f76860084c
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-04-17 15:01:11 -0300

    change filReader

--- filereader/src/main/java/fileReader/FileReaderPS.java
+++ filereader/src/main/java/fileReader/FileReaderPS.java
@@ -2,20 +2,31 @@ package fileReader;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Path;
 import java.util.stream.Collectors;
+import lombok.*;
 
+@Data
+@Builder
+@NoArgsConstructor
 public class FileReaderPS {
 
-  public static String readFile(File file) {
-    BufferedReader br = null;
-    try {
-      br = new BufferedReader(new FileReader(file));
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-    assert br != null;
+  @SneakyThrows
+  public static String readFile(String filePath) {
+    val br = new BufferedReader(new FileReader(filePath));
     return br.lines().collect(Collectors.joining("\n"));
+  }
+
+  @SneakyThrows
+  public static String readFile(File file) {
+    val br = new BufferedReader(new FileReader(file));
+    return br.lines().collect(Collectors.joining("\n"));
+  }
+
+  @SneakyThrows
+  public static String readFile(Path path) {
+    final var file = path.toFile();
+    return readFile(file);
   }
 }
